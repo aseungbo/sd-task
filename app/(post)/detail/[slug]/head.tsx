4350e7d0a9e8de59e6abb92443863b28dca2c3a2
seungb5 <seungbo1112@gmail.com>
2023-02-28 18:26:49 +0900

    fix: Modified head title && Delete asset

--- app/(post)/detail/[slug]/head.tsx
+++ app/(post)/detail/[slug]/head.tsx
@@ -1,9 +1,26 @@
-export default function Head() {
+interface HeadProps {
+  params: {
+    slug: number;
+  };
+}
+
+/**
+ * TODO
+ * - title에 post.title 적용
+ * @param props
+ * @returns
+ */
+export default function Head(props: HeadProps) {
+  const { params } = props;
+
   return (
     <>
-      <title>detail</title>
+      <title>{`Detail ${params.slug}`}</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <meta name="description" content="More details on the post" />
+      <meta
+        name="description"
+        content="게시글에 대한 상세정보를 볼 수 있는 페이지."
+      />
       <link rel="icon" href="/favicon.ico" />
     </>
   );
